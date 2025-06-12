632be42767f369a7b5697b876c5a288cc75a80d6
Sean Carter <collisteru@gmail.com>
2025-06-12 10:37:11 -0600

    Page now displays on client and server alike.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -7,9 +8,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); // parse JSON body
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from Express!" });
+});
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
