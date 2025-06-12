31942c2d27843aea07e0986f07e92bda1298f369
Sean Carter <collisteru@gmail.com>
2025-06-12 11:36:43 -0600

    Added database

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,55 @@
-import { useEffect, useState } from "react";
+// client/src/App.js
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [msg, setMsg] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState({ title: "", body: "" });
 
   useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/hello")
-      .then((res) => setMsg(res.data.message))
-      .catch((err) => console.error(err));
+    axios.get("/api/notes").then((res) => setNotes(res.data));
   }, []);
 
-  return <h1>{msg || "Loading..."}</h1>;
+  const addNote = () => {
+    axios.post("/api/notes", newNote).then((res) => {
+      setNotes([...notes, res.data]);
+      setNewNote({ title: "", body: "" });
+    });
+  };
+
+  return (
+    <>
+      <span>Notes</span>
+      <div>
+        <div>Title:</div>
+        <input
+          value={newNote.title}
+          onChange={(e) => setNewNote({ ...newNote, title: e.target.value })}
+        />
+      </div>
+
+      <div>
+        <div>Body:</div>
+        <textarea
+          value={newNote.body}
+          onChange={(e) => setNewNote({ ...newNote, body: e.target.value })}
+        />
+      </div>
+
+      <button onClick={addNote}>Add Note</button>
+
+      <ul>
+        {[...notes]
+          .reverse()
+          .filter((n) => n.title?.trim() || n.body?.trim()) // keep only nonempty notes
+          .map((n) => (
+            <li key={n.id}>
+              <strong>{n.title}</strong>: {n.body}
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const { PrismaClient } = require("@prisma/client");
+
+// Prisma server
+const prisma = new PrismaClient();
+
 require("dotenv").config();
 
 const app = express();
@@ -16,6 +21,17 @@ app.get("/api/hello", (req, res) => {
 
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+app.get("/api/notes", async (req, res) => {
+  const notes = await prisma.note.findMany();
+  res.json(notes);
+});
+
+app.post("/api/notes", async (req, res) => {
+  const { title, body } = req.body;
+  const note = await prisma.note.create({ data: { title, body } });
+  res.status(201).json(note);
 });
 
 app.listen(PORT, () => {
