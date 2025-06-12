e20e43885e0d1cdd809560b8fa138ed13ec4167f
Sean Carter <collisteru@gmail.com>
2025-06-12 12:06:15 -0600

    Making UI Skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // client/src/App.js
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -19,35 +20,50 @@ function App() {
 
   return (
     <>
-      <span>Notes</span>
-      <div>
-        <div>Title:</div>
-        <input
-          value={newNote.title}
-          onChange={(e) => setNewNote({ ...newNote, title: e.target.value })}
-        />
+      <div className="app-container">
+        <header className="App-header">
+          <h1 className="App-title">ZoomLog</h1>
+          <a href="/" className="Header-link">
+            Home
+          </a>
+        </header>
+
+        <main className="App-main">
+          <h1>Welcome to the App</h1>
+
+          <span>Notes</span>
+          <div>
+            <div>Title:</div>
+            <input
+              value={newNote.title}
+              onChange={(e) =>
+                setNewNote({ ...newNote, title: e.target.value })
+              }
+            />
+          </div>
+
+          <div>
+            <div>Body:</div>
+            <textarea
+              value={newNote.body}
+              onChange={(e) => setNewNote({ ...newNote, body: e.target.value })}
+            />
+          </div>
+
+          <button onClick={addNote}>Add Note</button>
+
+          <ul>
+            {[...notes]
+              .reverse()
+              .filter((n) => n.title?.trim() || n.body?.trim()) // keep only nonempty notes
+              .map((n) => (
+                <li key={n.id}>
+                  <strong>{n.title}</strong>: {n.body}
+                </li>
+              ))}
+          </ul>
+        </main>
       </div>
-
-      <div>
-        <div>Body:</div>
-        <textarea
-          value={newNote.body}
-          onChange={(e) => setNewNote({ ...newNote, body: e.target.value })}
-        />
-      </div>
-
-      <button onClick={addNote}>Add Note</button>
-
-      <ul>
-        {[...notes]
-          .reverse()
-          .filter((n) => n.title?.trim() || n.body?.trim()) // keep only nonempty notes
-          .map((n) => (
-            <li key={n.id}>
-              <strong>{n.title}</strong>: {n.body}
-            </li>
-          ))}
-      </ul>
     </>
   );
 }
